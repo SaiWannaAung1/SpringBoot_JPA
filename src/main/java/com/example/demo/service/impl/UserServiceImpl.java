243c9905e606118d0bd936f4f78411f7867ed50f
SaiWannaAung <saiwannaaung095@gmail.com>
2019-10-27 09:48:30 +0630

    SpringBoot JPA

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -35,7 +35,7 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public void save(UserDto dto) {
-		
+//		/
 		User user = dto.getEntity();
 		this.userJpaRepsitory.save(user);
 	}
